081536771b52a1a074b94176e0ca9ef3763f5ab7
revvine1 <vineet.revanker@optelgroup.com>
2018-04-10 10:19:51 +0530

    Added Conftest.py and config.cfg files to the repo

--- Vineet's Automation Research/conftest.py
+++ Vineet's Automation Research/conftest.py
@@ -1,5 +1,19 @@
 import pytest
+from selenium import webdriver
 
 @pytest.fixture
 def pytest_runtest_setup():
     return "Conftest.py: setting up fixture values"
+
+@pytest.fixture
+def invoke_driver_instance(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "firefox":
+        driver = webdriver.Firefox()
+    elif request.param == "ie":
+        driver = webdriver.Ie()
+    yield driver
+    print("Trearing down function")
+    driver.close()
+    driver.quit()
--- Vineet's Automation Research/test.py
+++ Vineet's Automation Research/test.py
@@ -1,7 +1,30 @@
 # FIRST PYTHON SCRIPT
+# IMPORTS
+import pytest
+from selenium import webdriver
+from configparser import ConfigParser
 
-def test_1_check_git_and_working(pytest_runtest_setup):
+
+
+@pytest.mark.parametrize('invoke_driver_instance', ["chrome"], indirect=True)
+def test_1_check_git_and_working(pytest_runtest_setup, invoke_driver_instance):
+    invoke_driver_instance.get("http://www.istqb.in/") #TODO: PUT THIS IN AN EXTERNAL DATA FILES
     print("\n***********")
     print("TEST: Calling fixture")
     print(pytest_runtest_setup)
     print("***********")
+
+# def test_2_get_config_value():
+#     parser = ConfigParser()
+#     parser.read('config.cfg')
+#     print(parser.get('ENVIRONMENT', 'env'))
+
+# def test_3_get_all_config_data():
+#     parser = ConfigParser()
+#     parser.read('config.cfg')
+#     for section_name in parser.sections():
+#         print("Section: {0}".format(section_name))
+#         # print('Section:', section_name)
+#         print("Options: {0}".format(parser.options(section_name)))
+#         for name, value in parser.items(section_name):
+#             print('{0} = {1}'.format(name, value))
